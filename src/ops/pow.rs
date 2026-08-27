41d155541928e901523b1d81600d5b74aaa2b0b7
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2026-08-27 22:38:33 +0200

    Implement `Pow` and `CheckedPow` for more combinations of operand types

--- src/ops/pow.rs
+++ src/ops/pow.rs
@@ -14,79 +14,141 @@ pub trait CheckedPow<Rhs> {
     fn checked_pow(self, rhs: Rhs) -> Option<Self::Output>;
 }
 
-macro_rules! impl_pow_traits_for_ints {
-    ($($ty:ty),*) => {
-        $(
-            impl Pow<u32> for $ty {
-                type Output = $ty;
+macro_rules! impl_pow_for_int {
+    ($t:ty, $rhs:ty) => {
+        impl Pow<$rhs> for $t {
+            type Output = $t;
 
-                #[inline]
-                fn pow(self, rhs: u32) -> Self::Output {
-                    <$ty>::pow(self, rhs)
-                }
+            #[inline]
+            fn pow(self, rhs: $rhs) -> $t {
+                //<$t>::pow(
+                //self,
+                //rhs.try_into()
+                //.expect("exponent out of range for integer `pow`"),
+                //)
+                <$t>::pow(self, u32::from(rhs))
             }
+        }
 
-            impl CheckedPow<u32> for $ty {
-                type Output = $ty;
+        impl CheckedPow<$rhs> for $t {
+            type Output = $t;
 
-                #[inline]
-                fn checked_pow(self, rhs: u32) -> Option<Self::Output> {
-                    <$ty>::checked_pow(self, rhs)
-                }
+            #[inline]
+            fn checked_pow(self, rhs: $rhs) -> Option<$t> {
+                //rhs.try_into()
+                //.ok()
+                //.and_then(|exp| <$t>::checked_pow(self, exp))
+                <$t>::checked_pow(self, u32::from(rhs))
             }
-        )*
+        }
+    };
+}
+
+macro_rules! impl_pow_for_int_for_all_rhs {
+    ($t:ty) => {
+        impl_pow_for_int!($t, u8);
+        impl_pow_for_int!($t, u16);
+        impl_pow_for_int!($t, u32);
+        //impl_pow_for_int!($t, u64);
+        //impl_pow_for_int!($t, u128);
+        //impl_pow_for_int!($t, usize);
+        //impl_pow_for_int!($t, i8);
+        //impl_pow_for_int!($t, i16);
+        //impl_pow_for_int!($t, i32);
+        //impl_pow_for_int!($t, i64);
+        //impl_pow_for_int!($t, i128);
+        //impl_pow_for_int!($t, isize);
+    };
+}
+
+impl_pow_for_int_for_all_rhs!(u8);
+impl_pow_for_int_for_all_rhs!(u16);
+impl_pow_for_int_for_all_rhs!(u32);
+impl_pow_for_int_for_all_rhs!(u64);
+impl_pow_for_int_for_all_rhs!(u128);
+impl_pow_for_int_for_all_rhs!(usize);
+impl_pow_for_int_for_all_rhs!(i8);
+impl_pow_for_int_for_all_rhs!(i16);
+impl_pow_for_int_for_all_rhs!(i32);
+impl_pow_for_int_for_all_rhs!(i64);
+impl_pow_for_int_for_all_rhs!(i128);
+impl_pow_for_int_for_all_rhs!(isize);
+
+#[cfg(feature = "std")]
+macro_rules! impl_pow_for_float {
+    ($t:ty, $rhs:ty) => {
+        impl Pow<$rhs> for $t {
+            type Output = $t;
+
+            #[inline]
+            fn pow(self, rhs: $rhs) -> $t {
+                //<$t>::powi(
+                //self,
+                //rhs.try_into().expect("exponent out of range for `powi`"),
+                //)
+                <$t>::powi(self, i32::from(rhs))
+            }
+        }
+
+        impl CheckedPow<$rhs> for $t {
+            type Output = $t;
+
+            #[inline]
+            fn checked_pow(self, rhs: $rhs) -> Option<$t> {
+                //let result = rhs.try_into().ok().map(|exp| <$t>::powi(self, exp))?;
+                let result = <$t>::powi(self, i32::from(rhs));
+
+                result.is_finite().then_some(result)
+            }
+        }
     };
 }
 
 #[cfg(feature = "std")]
-macro_rules! impl_pow_traits_for_floats {
-    ($($ty:ty),*) => {
-        $(
-            impl Pow<i32> for $ty {
-                type Output = $ty;
+macro_rules! impl_pow_for_float_using_powf {
+    ($t:ty) => {
+        impl Pow<$t> for $t {
+            type Output = $t;
 
-                #[inline]
-                fn pow(self, rhs: i32) -> Self::Output {
-                    <$ty>::powi(self, rhs)
-                }
+            #[inline]
+            fn pow(self, rhs: $t) -> $t {
+                <$t>::powf(self, rhs)
             }
+        }
 
-            impl CheckedPow<i32> for $ty {
-                type Output = $ty;
+        impl CheckedPow<$t> for $t {
+            type Output = $t;
 
-                #[inline]
-                fn checked_pow(self, rhs: i32) -> Option<Self::Output> {
-                    let result = <$ty>::powi(self, rhs);
+            #[inline]
+            fn checked_pow(self, rhs: $t) -> Option<$t> {
+                let result = <$t>::powf(self, rhs);
 
-                    result.is_finite().then_some(result)
-                }
+                result.is_finite().then_some(result)
             }
-
-            impl Pow<$ty> for $ty {
-                type Output = $ty;
-
-                #[inline]
-                fn pow(self, rhs: $ty) -> Self::Output {
-                    <$ty>::powf(self, rhs)
-                }
-            }
-
-            impl CheckedPow<$ty> for $ty {
-                type Output = $ty;
-
-                #[inline]
-                fn checked_pow(self, rhs: $ty) -> Option<Self::Output> {
-                    let result = <$ty>::powf(self, rhs);
-
-                    result.is_finite().then_some(result)
-                }
-            }
-        )*
+        }
     };
 }
 
-impl_pow_traits_for_ints!(i8, i16, i32, i64, i128, isize);
-impl_pow_traits_for_ints!(u8, u16, u32, u64, u128, usize);
+#[cfg(feature = "std")]
+macro_rules! impl_pow_for_float_for_all_rhs {
+    ($t:ty) => {
+        impl_pow_for_float!($t, i8);
+        impl_pow_for_float!($t, u8);
+        impl_pow_for_float!($t, i16);
+        impl_pow_for_float!($t, u16);
+        impl_pow_for_float!($t, i32);
+        //impl_pow_for_float!($t, u32);
+        //impl_pow_for_float!($t, i64);
+        //impl_pow_for_float!($t, u64);
+        //impl_pow_for_float!($t, i128);
+        //impl_pow_for_float!($t, u128);
+        //impl_pow_for_float!($t, isize);
+        //impl_pow_for_float!($t, usize);
+        impl_pow_for_float_using_powf!($t);
+    };
+}
 
 #[cfg(feature = "std")]
-impl_pow_traits_for_floats!(f32, f64);
+impl_pow_for_float_for_all_rhs!(f32);
+#[cfg(feature = "std")]
+impl_pow_for_float_for_all_rhs!(f64);
