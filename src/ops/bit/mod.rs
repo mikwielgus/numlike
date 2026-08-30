a9205ee8029946a54d7363e275c7af2387d0e9c8
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2026-08-30 11:00:27 +0200

    Add `OverflowingShl`, `OverflowingShr` traits

--- src/ops/bit/mod.rs
+++ src/ops/bit/mod.rs
@@ -3,9 +3,11 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 mod checked;
+mod overflowing;
 mod plain;
 mod wrapping;
 
 pub use checked::*;
+pub use overflowing::*;
 pub use plain::*;
 pub use wrapping::*;
---
+++ src/ops/bit/overflowing.rs
@@ -0,0 +1,42 @@
+// SPDX-FileCopyrightText: 2026 numlike contributors
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+pub trait OverflowingShl {
+    type Output;
+
+    fn overflowing_shl(self, rhs: u32) -> (Self::Output, bool);
+}
+
+pub trait OverflowingShr {
+    type Output;
+
+    fn overflowing_shr(self, rhs: u32) -> (Self::Output, bool);
+}
+
+macro_rules! impl_overflowing_shift_traits_for_ints {
+    ($($ty:ty),*) => {
+        $(
+            impl OverflowingShl for $ty {
+                type Output = $ty;
+
+                #[inline]
+                fn overflowing_shl(self, rhs: u32) -> (Self::Output, bool) {
+                    <$ty>::overflowing_shl(self, rhs)
+                }
+            }
+
+            impl OverflowingShr for $ty {
+                type Output = $ty;
+
+                #[inline]
+                fn overflowing_shr(self, rhs: u32) -> (Self::Output, bool) {
+                    <$ty>::overflowing_shr(self, rhs)
+                }
+            }
+        )*
+    };
+}
+
+impl_overflowing_shift_traits_for_ints!(i8, i16, i32, i64, i128, isize);
+impl_overflowing_shift_traits_for_ints!(u8, u16, u32, u64, u128, usize);
