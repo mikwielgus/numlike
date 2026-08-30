041b84067e21ff50481c0f6f2c5ce97cf3471fd5
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2026-08-30 12:33:41 +0200

    Add hyperbolic functions and implement them for `f32`, `f64`

---
+++ src/fns/hyp.rs
@@ -0,0 +1,110 @@
+// SPDX-FileCopyrightText: 2026 numlike contributors
+//
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+pub trait FullHypFns<Rhs = Self>: HypFns + InvHypFns<Rhs> {}
+impl<Rhs, T: HypFns + InvHypFns<Rhs>> FullHypFns<Rhs> for T {}
+
+pub trait HypFns: Sinh + Cosh + Tanh {}
+impl<T: Sinh + Cosh + Tanh> HypFns for T {}
+
+pub trait Sinh {
+    type Output;
+
+    fn sinh(self) -> Self::Output;
+}
+
+pub trait Cosh {
+    type Output;
+
+    fn cosh(self) -> Self::Output;
+}
+
+pub trait Tanh {
+    type Output;
+
+    fn tanh(self) -> Self::Output;
+}
+
+pub trait InvHypFns<Rhs = Self>: Asinh + Acosh + Atanh {}
+impl<Rhs, T: Asinh + Acosh + Atanh> InvHypFns<Rhs> for T {}
+
+pub trait Asinh {
+    type Output;
+
+    fn asinh(self) -> Self::Output;
+}
+
+pub trait Acosh {
+    type Output;
+
+    fn acosh(self) -> Self::Output;
+}
+
+pub trait Atanh {
+    type Output;
+
+    fn atanh(self) -> Self::Output;
+}
+
+macro_rules! impl_trig_traits_for_floats {
+    ($($ty:ty),*) => {
+        $(
+            impl Sinh for $ty {
+                type Output = $ty;
+
+                #[inline]
+                fn sinh(self) -> Self::Output {
+                    <$ty>::sinh(self)
+                }
+            }
+
+            impl Cosh for $ty {
+                type Output = $ty;
+
+                #[inline]
+                fn cosh(self) -> Self::Output {
+                    <$ty>::cosh(self)
+                }
+            }
+
+            impl Tanh for $ty {
+                type Output = $ty;
+
+                #[inline]
+                fn tanh(self) -> Self::Output {
+                    <$ty>::tanh(self)
+                }
+            }
+
+            impl Asinh for $ty {
+                type Output = $ty;
+
+                #[inline]
+                fn asinh(self) -> Self::Output {
+                    <$ty>::asin(self)
+                }
+            }
+
+            impl Acosh for $ty {
+                type Output = $ty;
+
+                #[inline]
+                fn acosh(self) -> Self::Output {
+                    <$ty>::acos(self)
+                }
+            }
+
+            impl Atanh for $ty {
+                type Output = $ty;
+
+                #[inline]
+                fn atanh(self) -> Self::Output {
+                    <$ty>::atan(self)
+                }
+            }
+        )*
+    }
+}
+
+impl_trig_traits_for_floats!(f32, f64);
--- src/fns/mod.rs
+++ src/fns/mod.rs
@@ -2,10 +2,12 @@
 //
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+mod hyp;
 mod pow;
 mod sign;
 mod trig;
 
+pub use hyp::*;
 pub use pow::*;
 pub use sign::*;
 pub use trig::*;
