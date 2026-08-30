01f02b3a5439051c9a244a1b096542477971ab2f
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2026-08-31 00:34:41 +0200

    Add checked exponential functions

--- src/fns/exp.rs
+++ src/fns/exp.rs
@@ -22,6 +22,28 @@ pub trait ExpM1 {
 
     fn exp_m1(self) -> Self::Output;
 }
+
+pub trait CheckedExpFns: CheckedExp + CheckedExp2 + CheckedExpM1 {}
+impl<T: CheckedExp + CheckedExp2 + CheckedExpM1> CheckedExpFns for T {}
+
+pub trait CheckedExp {
+    type Output;
+
+    fn checked_exp(self) -> Option<Self::Output>;
+}
+
+pub trait CheckedExp2 {
+    type Output;
+
+    fn checked_exp2(self) -> Option<Self::Output>;
+}
+
+pub trait CheckedExpM1 {
+    type Output;
+
+    fn checked_exp_m1(self) -> Option<Self::Output>;
+}
+
 macro_rules! impl_exp_traits_for_floats {
     ($($ty:ty),*) => {
         $(
@@ -49,6 +71,39 @@ macro_rules! impl_exp_traits_for_floats {
                 #[inline]
                 fn exp_m1(self) -> Self::Output {
                     <$ty>::exp_m1(self)
+                }
+            }
+
+            impl CheckedExp for $ty {
+                type Output = $ty;
+
+                #[inline]
+                fn checked_exp(self) -> Option<Self::Output> {
+                    let result = <$ty>::exp(self);
+
+                    result.is_finite().then_some(result)
+                }
+            }
+
+            impl CheckedExp2 for $ty {
+                type Output = $ty;
+
+                #[inline]
+                fn checked_exp2(self) -> Option<Self::Output> {
+                    let result = <$ty>::exp2(self);
+
+                    result.is_finite().then_some(result)
+                }
+            }
+
+            impl CheckedExpM1 for $ty {
+                type Output = $ty;
+
+                #[inline]
+                fn checked_exp_m1(self) -> Option<Self::Output> {
+                    let result = <$ty>::exp_m1(self);
+
+                    result.is_finite().then_some(result)
                 }
             }
         )*
