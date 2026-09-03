7e62c049ebcee2d69a429735448e0f2e12c4c3dd
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2026-09-03 15:30:26 +0200

    Add doc comments for `convert.rs` traits

--- src/convert.rs
+++ src/convert.rs
@@ -2,10 +2,26 @@
 //
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+/// Converts a value from one type to another.
+///
+/// Internally, `as` operator is used to convert between Rust primitives. For
+/// non-exact conversion where a non-primitive type are involved, rounding
+/// behavior is left to the implementors, but consistency with `as` is
+/// encouraged.
 pub trait CastFrom<T> {
     fn cast_from(value: T) -> Self;
 }
 
+/// Converts a value from one type to another.
+///
+/// Internally, `as` operator is used to convert between Rust primitives. For
+/// non-exact conversion where a non-primitive type are involved, rounding
+/// behavior is left to the implementors, but consistency with `as` is
+/// encouraged.
+///
+/// Just as with Rust's standard library's `Into`, It is recommended to not
+/// implement this trait directly, as it already has a blanket implementation
+/// for types that implement `CastFrom`.
 pub trait CastInto<T> {
     fn cast_into(self) -> T;
 }
