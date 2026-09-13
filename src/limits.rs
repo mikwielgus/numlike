08ce950c510af14e5c01daec74e65f2de65f2d39
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2026-09-13 23:07:58 +0200

    Add `MinExactInteger` and `MaxExactInteger` limits traits

--- src/limits.rs
+++ src/limits.rs
@@ -28,9 +28,49 @@ pub trait MaxExtended {
     const MAX_EXTENDED: Self;
 }
 
+/// Minimum integer that can be represented exactly in this type, with no other
+/// integer converting to the same value.
+///
+/// For an integer `x` which satisfies
+/// `MIN_EXACT_INTEGER <= x <= MAX_EXACT_INTEGER`, there is a one-to-one mapping
+/// between that integer and a value of this type. `MAX_EXACT_INTEGER + 1` also
+/// converts losslessly, but `MAX_EXACT_INTEGER + 2` converts to the same value
+/// as `MAX_EXACT_INTEGER + 1`, so there is not a one-to-one mapping.
+///
+/// For integer types this is the same as the type's minimum. For floating point
+/// types this is equal to `1 - 2^MANTISSA_DIGITS` (that is, `-MAX_EXACT_INTEGER`).
+pub trait MinExactInteger {
+    /// Minimum integer that can be represented exactly in this type, with no
+    /// other integer converting to the same value.
+    const MIN_EXACT_INTEGER: Self;
+}
+
+/// Maximum integer that can be represented exactly in this type, with no other
+/// integer converting to the same value.
+///
+/// For an integer `x` which satisfies
+/// `MIN_EXACT_INTEGER <= x <= MAX_EXACT_INTEGER`, there is a one-to-one mapping
+/// between that integer and a value of this type. `MAX_EXACT_INTEGER + 1` also
+/// converts losslessly, but `MAX_EXACT_INTEGER + 2` converts to the same value
+/// as `MAX_EXACT_INTEGER + 1`, so there is not a one-to-one mapping.
+///
+/// For integer types this is the same as the type's maximum. For floating point
+/// types this is equal to `2^MANTISSA_DIGITS - 1`.
+pub trait MaxExactInteger {
+    /// Maximum integer that can be represented exactly in this type, with no
+    /// other integer converting to the same value.
+    const MAX_EXACT_INTEGER: Self;
+}
+
 /// Bundle of limits for a numeric type.
-pub trait Limits: MinFinite + MaxFinite + MinExtended + MaxExtended {}
-impl<T: MinFinite + MaxFinite + MinExtended + MaxExtended> Limits for T {}
+pub trait Limits:
+    MinFinite + MaxFinite + MinExtended + MaxExtended + MinExactInteger + MaxExactInteger
+{
+}
+impl<T: MinFinite + MaxFinite + MinExtended + MaxExtended + MinExactInteger + MaxExactInteger>
+    Limits for T
+{
+}
 
 macro_rules! impl_limits_traits_for_ints {
     ($($ty:ty),*) => {
@@ -49,6 +89,14 @@ macro_rules! impl_limits_traits_for_ints {
 
             impl MaxExtended for $ty {
                 const MAX_EXTENDED: Self = <$ty>::MAX;
+            }
+
+            impl MinExactInteger for $ty {
+                const MIN_EXACT_INTEGER: Self = <$ty>::MIN;
+            }
+
+            impl MaxExactInteger for $ty {
+                const MAX_EXACT_INTEGER: Self = <$ty>::MAX;
             }
         )*
     };
@@ -71,6 +119,16 @@ macro_rules! impl_limits_traits_for_floats {
 
             impl MaxExtended for $ty {
                 const MAX_EXTENDED: Self = <$ty>::INFINITY;
+            }
+
+            impl MaxExactInteger for $ty {
+                const MAX_EXACT_INTEGER: Self =
+                    ((1i128 << (<$ty>::MANTISSA_DIGITS as u32)) - 1) as Self;
+            }
+
+            impl MinExactInteger for $ty {
+                const MIN_EXACT_INTEGER: Self =
+                    -<$ty as MaxExactInteger>::MAX_EXACT_INTEGER;
             }
         )*
     };
