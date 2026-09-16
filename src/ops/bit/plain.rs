bece69378a54eb4dd8c20d1eba99c546005e3783
Mikolaj Wielgus <wielgusmikolaj@gmail.com>
2026-09-16 02:12:41 +0200

    Add tests for bitwise operations (only plain ones for now)

--- src/ops/bit/plain.rs
+++ src/ops/bit/plain.rs
@@ -61,3 +61,149 @@ macro_rules! impl_not_assign_for_bitwisables {
 impl_not_assign_for_bitwisables!(i8, i16, i32, i64, i128, isize);
 impl_not_assign_for_bitwisables!(u8, u16, u32, u64, u128, usize);
 impl_not_assign_for_bitwisables!(bool);
+
+macro_rules! test_plain_bitshift_traits_int_nonnegative {
+    ($ty:ty, $tests_mod:ident) => {
+        #[cfg(test)]
+        mod $tests_mod {
+            use core::ops::{Shl, ShlAssign, Shr, ShrAssign};
+
+            use crate::elem::*;
+
+            #[test]
+            fn test_shl() {
+                let zero = <$ty as Zero>::ZERO;
+                let one = <$ty as One>::ONE;
+                let two = one + one;
+                let four = two + two;
+
+                assert_eq!(Shl::shl(zero, one), zero);
+                assert_eq!(Shl::shl(one, one), two);
+                assert_eq!(Shl::shl(two, one), four);
+                assert_eq!(Shl::shl(one, two), four);
+            }
+
+            #[test]
+            fn test_shr() {
+                let zero = <$ty as Zero>::ZERO;
+                let one = <$ty as One>::ONE;
+                let two = one + one;
+                let four = two + two;
+
+                assert_eq!(Shr::shr(zero, one), zero);
+                assert_eq!(Shr::shr(one, one), zero);
+                assert_eq!(Shr::shr(two, one), one);
+                assert_eq!(Shr::shr(four, one), two);
+                assert_eq!(Shr::shr(four, two), one);
+            }
+
+            #[test]
+            fn test_shl_assign() {
+                let one = <$ty as One>::ONE;
+                let two = one + one;
+                let four = two + two;
+                let mut value = one;
+
+                ShlAssign::shl_assign(&mut value, one);
+                assert_eq!(value, two);
+
+                ShlAssign::shl_assign(&mut value, one);
+                assert_eq!(value, four);
+            }
+
+            #[test]
+            fn test_shr_assign() {
+                let one = <$ty as One>::ONE;
+                let two = one + one;
+                let four = two + two;
+                let mut value = four;
+
+                ShrAssign::shr_assign(&mut value, one);
+                assert_eq!(value, two);
+
+                ShrAssign::shr_assign(&mut value, one);
+                assert_eq!(value, one);
+            }
+        }
+    };
+}
+
+macro_rules! test_plain_bitshift_traits_int_negative {
+    ($ty:ty, $tests_mod:ident) => {
+        #[cfg(test)]
+        mod $tests_mod {
+            use core::ops::{Shl, ShlAssign, Shr, ShrAssign};
+
+            use crate::elem::*;
+
+            #[test]
+            fn test_shl() {
+                let one = <$ty as One>::ONE;
+                let two = one + one;
+                let four = two + two;
+
+                assert_eq!(Shl::shl(-one, one), -two);
+                assert_eq!(Shl::shl(-two, one), -four);
+            }
+
+            #[test]
+            fn test_shr() {
+                let one = <$ty as One>::ONE;
+                let two = one + one;
+                let four = two + two;
+
+                assert_eq!(Shr::shr(-two, one), -one);
+                assert_eq!(Shr::shr(-four, one), -two);
+                assert_eq!(Shr::shr(-four, two), -one);
+            }
+
+            #[test]
+            fn test_shl_assign() {
+                let one = <$ty as One>::ONE;
+                let two = one + one;
+                let four = two + two;
+                let mut value = -one;
+
+                ShlAssign::shl_assign(&mut value, one);
+                assert_eq!(value, -two);
+
+                ShlAssign::shl_assign(&mut value, one);
+                assert_eq!(value, -four);
+            }
+
+            #[test]
+            fn test_shr_assign() {
+                let one = <$ty as One>::ONE;
+                let two = one + one;
+                let four = two + two;
+                let mut value = -four;
+
+                ShrAssign::shr_assign(&mut value, one);
+                assert_eq!(value, -two);
+
+                ShrAssign::shr_assign(&mut value, one);
+                assert_eq!(value, -one);
+            }
+        }
+    };
+}
+
+test_plain_bitshift_traits_int_nonnegative!(i8, i8_nonnegative_tests);
+test_plain_bitshift_traits_int_negative!(i8, i8_negative_tests);
+test_plain_bitshift_traits_int_nonnegative!(i16, i16_nonnegative_tests);
+test_plain_bitshift_traits_int_negative!(i16, i16_negative_tests);
+test_plain_bitshift_traits_int_nonnegative!(i32, i32_nonnegative_tests);
+test_plain_bitshift_traits_int_negative!(i32, i32_negative_tests);
+test_plain_bitshift_traits_int_nonnegative!(i64, i64_nonnegative_tests);
+test_plain_bitshift_traits_int_negative!(i64, i64_negative_tests);
+test_plain_bitshift_traits_int_nonnegative!(i128, i128_nonnegative_tests);
+test_plain_bitshift_traits_int_negative!(i128, i128_negative_tests);
+test_plain_bitshift_traits_int_nonnegative!(isize, isize_nonnegative_tests);
+test_plain_bitshift_traits_int_negative!(isize, isize_negative_tests);
+
+test_plain_bitshift_traits_int_nonnegative!(u8, u8_tests);
+test_plain_bitshift_traits_int_nonnegative!(u16, u16_tests);
+test_plain_bitshift_traits_int_nonnegative!(u32, u32_tests);
+test_plain_bitshift_traits_int_nonnegative!(u64, u64_tests);
+test_plain_bitshift_traits_int_nonnegative!(u128, u128_tests);
+test_plain_bitshift_traits_int_nonnegative!(usize, usize_tests);
